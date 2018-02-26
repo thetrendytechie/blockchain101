71cd2f12f1313686c0fddb393c75d07ab5a70c5a
Sage Franch <trendytechie@live.com>
2018-02-26 12:22:06 -0500

    Added comments to smart contract

--- Hello/Hello.sol
+++ Hello/Hello.sol
@@ -1,19 +1,22 @@
+//specify solidity version
 pragma solidity ^0.4.13;
 
 contract Hello {
+  //this will hold our greeting text
   string greeting;
 
   function Hello() public {
-    //constructor
+    //constructor to initialize default greeting to hello
     greeting = "hello";
   }
 
   function getGreeting() public view returns (string) {
+    //return the greeting
     return greeting;
-
   }
 
   function setGreeting(string _greeting) public {
+    //set the greeting to user-provided input
     greeting = _greeting;
   }
 }
